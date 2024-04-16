0e22b0d6736854954a44eaf1c91ebe4c10ea61a6
gm-m <gmaruca77@gmail.com>
2024-04-16 23:03:54 +0200

    feat: ShadowDOM
    Isolate extension HTML and CSS from target webpage.

--- content.js
+++ content.js
@@ -6,15 +6,23 @@ function openPopup() {
     if (!searchEngines.alias) return;
   });
 
+  const popupContainer = document.createElement('div');
+  popupContainer.attachShadow({ mode: 'open' });
+  if (popupContainer.shadowRoot) {
+    popupContainer.shadowRoot.innerHTML = `<style>:host {all: initial;}</style>`
+  }
+
   const popup = document.createElement('div');
   popup.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); width:50%; z-index: 99"
   popup.innerHTML = `
-    <div id="cstm-search">
-      <input type="text" id="user-input" placeholder="Search..." autocomplete="off" style="width: 100%; padding: 10px; color: black; background-color: white; border: 1px solid black; font-family: sans-serif; font-size: medium; outline: none;">
-      <span id="active-alias">No match found</span>
-    </div>
+      <div id="cstm-search">
+        <input type="text" id="user-input" placeholder="Search..." autocomplete="off" style="width: 100%; padding: 10px; color: black; background-color: white; border: 1px solid black; font-family: sans-serif; font-size: medium; outline: none;">
+        <span id="active-alias">No match found</span>
+      </div>
   `;
-  document.body.appendChild(popup);
+
+  popupContainer.shadowRoot.appendChild(popup);
+  document.body.appendChild(popupContainer);
 
   const userInputElement = popup.querySelector('#user-input');
   userInputElement.focus();
@@ -24,16 +32,16 @@ function openPopup() {
     const alias = inputText.substring(0, inputText.indexOf(' '));
     const previewAlias = alias && searchEngines.alias && searchEngines.alias.hasOwnProperty(alias) ? `${searchEngines.alias[alias].searchEngine} | Target: ${searchEngines.targetWindow}` : 'No match found';
 
-    document.getElementById('active-alias').innerText = previewAlias;
+    popup.querySelector('#active-alias').innerText = previewAlias;
   });
 
   this.addEventListener("keydown", (event) => {
     if (event.key === 'Escape') popup.remove();
   });
 
-  document.getElementById('cstm-search').addEventListener('keypress', function (e) {
+  popup.querySelector("#cstm-search").addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-      const userInput = document.getElementById('user-input').value;
+      const userInput = popup.querySelector('#user-input').value;
       const alias = userInput.substring(0, userInput.indexOf(' '));
       const searchQuery = userInput.substring(alias.length).trim();
       if (!searchQuery) return;
